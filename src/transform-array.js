c1bb96a93cf9e27ea28df854eb4b94abe8ae9ea6
Artem <75018018+rocks-py@users.noreply.github.com>
2022-04-29 07:02:23 -0700

    feat: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!")
+  let res = [...arr]
+
+  for (let i = 0; i < arr.length; i++) {
+    const elem = arr[i];
+    switch (elem) {
+      case '--double-next':
+        if (i + 1 < arr.length) res.splice(i + 1, 0, arr[i + 1]);
+        break;
+      case '--discard-next':
+        if (i + 1 < arr.length) res.splice(i + 1, 1);
+        break;
+        // Нужно проверять что элемент i - 2 не является '--discard-next' и только в этом случае что-то делать
+      case '--discard-prev':
+        if (i === 1 || (i >= 2 && arr[i - 2] !== '--discard-next')) res.splice(i - 1, 1);
+        break;
+      case '--double-prev':
+        if (i === 1 || (i >= 2 && arr[i - 2] !== '--discard-next')) res.splice(i - 1, 0, arr[i - 1]);
+        break;
+    }
+  }
+
+  const toRemove = ['--double-next', '--discard-next', '--discard-prev', '--double-prev']
+  res = res.filter( function( el ) {
+    return toRemove.indexOf( el ) < 0;
+  });
+
+  return res;
 }
 
 module.exports = {
   transform
 };
+
+// console.log(transform([1, 2, 3, '--double-next', 4, 5]));
+
+// console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]));
+// console.log(transform([1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5]));
+// console.log(transform([1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5]));
+// console.log(transform([1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5]));
