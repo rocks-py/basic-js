00c5ba5b9e673673c4bde196adf392e22ffff3e1
Artem <75018018+rocks-py@users.noreply.github.com>
2022-04-29 04:48:40 -0700

    feat: solve carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,28 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (typeof sampleActivity !== 'string' || isNaN(sampleActivity)) return false;
+  let activity = parseFloat(sampleActivity)
+  if (activity <= 0 || activity > MODERN_ACTIVITY || isNaN(activity)) return false;
+
+  let age = HALF_LIFE_PERIOD / 0.693 * Math.log(MODERN_ACTIVITY / activity);
+
+  return Math.ceil(age);
 }
 
 module.exports = {
   dateSample
 };
+
+// 12926
+console.log(dateSample('1'))
+
+
+console.log(dateSample('9000'))
+console.log(dateSample('15.1'))
+console.log(dateSample('0'))
+console.log(dateSample('-5'))
+console.log(dateSample('-55.8'))
