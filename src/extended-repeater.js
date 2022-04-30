061821581021479f4efe10aa89a0a9a35cb35a79
Artem <75018018+rocks-py@users.noreply.github.com>
2022-04-30 05:12:58 -0700

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let repeatTimes = options.repeatTimes !== undefined ? options.repeatTimes : 1;
+  let additionRepeatTimes = options.additionRepeatTimes !== undefined ? options.additionRepeatTimes : 1;
+  let separator = options.separator !== undefined ? options.separator : '+';
+  let additionSeparator = options.additionSeparator !== undefined ? options.additionSeparator : '|';
+  if (typeof str !== 'string') str = new String(str);
+  let addition = options.addition !== undefined ? new String(options.addition) : '';
+
+  let add = (addition + additionSeparator).repeat(additionRepeatTimes);
+  add = add.slice(0, add.length - additionSeparator.length);
+
+  let val = (str + add + separator).repeat(repeatTimes);
+  val = val.slice(0, val.length - separator.length);
+
+  return val;
 }
 
 module.exports = {
   repeater
 };
+
+// TESTstrADD!
+// console.log(repeater('TESTstr', { separator: 'ds', addition: 'ADD!', additionSeparator: ')))000' }));
