bb6defd95094fbaa47aaba81ad79680d71fedf5a
Artem <75018018+rocks-py@users.noreply.github.com>
2022-04-30 04:39:37 -0700

    feat: solve recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let max_depth = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        let depth = this.calculateDepth(arr[i]);
+        if (depth > max_depth) max_depth = depth;
+      }
+    }
+    return max_depth + 1;
   }
 }
 
