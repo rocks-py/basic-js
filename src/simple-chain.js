de2ecdfdfefd014e27b721b2f958d96d075fa1ba
Artem <75018018+rocks-py@users.noreply.github.com>
2022-04-30 04:14:37 -0700

    feat: solve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,28 +5,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  _arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (value === undefined) value = ' ';
+    this._arr.push(value);
+    console.log(this._arr.map(x => `( ${x} )`).join('~~'))
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (isNaN(position) || !Number.isInteger(position) || (position < 1 || position > this._arr.length)) 
+    {
+      this._arr = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this._arr.splice(position - 1, 1);
+    return this; 
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this._arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const res = this._arr.map(x => `( ${x} )`).join('~~');
+    this._arr = [];
+    return res;
   }
 };
 
 module.exports = {
   chainMaker
 };
+
+// '( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )'
+// console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain());
+
+// console.log(chainMaker.addLink(1).addLink(2).addLink(3).removeLink(0));
+// console.log(chainMaker.addLink(1).addLink(2).addLink(3).removeLink('2nd'));
+// console.log(chainMaker.addLink(1).addLink(2).addLink(3).removeLink(-2));
+// console.log(chainMaker.addLink(1).addLink(2).addLink(3).removeLink(4));
